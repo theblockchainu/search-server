542858105e9ce443557d33c35b35ecd504b3e8d1
donatedTunic1290 <connect@aakashbansal.com>
2017-07-28 19:59:10 -0700

    Updated all models to support /suggest API which will give results based on search term.

---
+++ server/mixins/remoteMethods.js
@@ -0,0 +1,36 @@
+'use strict';
+module.exports = function(Model, options) {
+  Model['suggestAll'] = function(field, query, cb) {
+    console.log('form suggestion query here');
+    var searchQuery  = {};
+    searchQuery.native = {};
+    searchQuery.native.suggest = {'suggests': {
+      'text': query,
+      'completion': {
+        'field': field,
+      },
+    }};
+    Model.find(searchQuery, function(err, results) {
+      if (!err) {
+        cb(null, results);
+      }      else {
+        console.trace(err.message);
+        if (err) {
+          return cb(err, null);
+        }
+      }
+    });
+  };
+
+  Model.remoteMethod(
+    'suggestAll',
+    {
+      accepts: [
+        {arg: 'field', type: 'string', required: true},
+        {arg: 'query', type: 'string', required: true},
+      ],
+      returns: {arg: 'suggestions', type: 'object', root: true},
+      http: {path: '/suggest', verb: 'get'},
+    }
+  );
+};
